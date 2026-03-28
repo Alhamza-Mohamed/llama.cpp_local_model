bb22b6599e3af16cc03767c72932bd94ead34a2f
Alhamza <hamza_tigerman@yahoo.com>
2026-03-28 21:49:39 +0200

    update pipeline

--- rag_core/pipeline.py
+++ rag_core/pipeline.py
@@ -1,20 +1,24 @@
 from typing import List, Dict
 
 class Pipeline:
-    def __init__(self, embedder, retriever, prompt_builder ):
+    def __init__(self, embedder, retriever, prompt_builder, llm ):
         self.embedder  = embedder
         self.retriever = retriever
-        self.prompt_builder = prompt_builder 
+        self.prompt_builder = prompt_builder
+        self.llm = llm  
          
-    def run(self, query: str, top_k: int = 5) -> List [Dict[str, str]]:
+    def run(self, query: str, top_k: int = 5) -> str:
         # 1- Embed query
         query_vector = self.embedder.embed(query)
 
         # 2- Retrieve relevant documents
         documents = self.retriever.retrieve(query_vector,top_k = top_k)
 
-        # 3- Build prompt
-        prompt = self.prompt_builder.build(query, documents)
+        # 3- Build message
+        messages = self.prompt_builder.build(query, documents)
 
-        # Return prompt (debug stage)
-        return prompt
+        # 4. Generate answer from LLM
+        answer = self.llm.generate(messages)
+
+         # 5. Return final answer
+        return answer
